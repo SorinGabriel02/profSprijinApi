3fe8cbc351b88fe7357154ebe513f3e2bc504b8a
SorinGabriel02 <lupu_sg@yahoo.com>
2023-04-18 16:37:50 +0300

    Rename AWS access variables

--- src/controllers/mediaController.js
+++ src/controllers/mediaController.js
@@ -4,8 +4,8 @@ const uuid = require("uuid").v4;
 
 const s3 = new AWS.S3({
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.APP_AWS_SECRET_ACCESS_KEY,
   },
   region: "eu-central-1",
 });
